180b9b96a76082039bbda277c2bae4a0e5109f50
PROJECT272024 <projectrelatedwork27@gmail.com>
2024-04-22 14:28:40 +0530

    changes for cors

--- middleware.js
+++ middleware.js
@@ -1,16 +1,21 @@
 export {default} from "next-auth/middleware";
-import { NextResponse } from 'next/server';
-import { parse } from 'cookie';
+import { NextResponse } from "next/server";
 
-/*export function middleware(request) {
-    if (request.nextUrl.pathname.startsWith('/_next')) {
-      const locale = request.nextUrl.locale || i18n.defaultLocale
-      request.nextUrl.searchParams.set('lang', locale)
-      request.nextUrl.href = request.nextUrl.href.replace(`/${locale}`, '')
-      return NextResponse.rewrite(request.nextUrl)
-    }
-    return NextResponse.next()
-  }*/
+export function middleware() {
+    // retrieve the current response
+    const res = NextResponse.next()
+
+    // add the CORS headers to the response
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
+}
 
 export const config = {matcher:["/homestay/:path*","/user/:path*"]};
 
