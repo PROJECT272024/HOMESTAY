8c7666c21cf31a74aceda58a6114055c37259585
PROJECT272024 <projectrelatedwork27@gmail.com>
2024-04-24 00:06:04 +0530

    changes made to check users

--- app/homestay/layout.jsx
+++ app/homestay/layout.jsx
@@ -1,8 +1,15 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import { getServerSession } from 'next-auth'
 import React from 'react'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
-const HomestayLayout = ({ children}) => {
+const HomestayLayout = async ({ children}) => {
+  const session = await getServerSession(authOptions)
+  if(!session || !(session?.user)){
+    redirect("/",'replace')
+  }
   return (
     <div className='flex flex-col min-h-screen relative'>
         <Navbar/>
