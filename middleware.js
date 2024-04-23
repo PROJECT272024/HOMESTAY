27dbb3c9c87c74ac4b35014185ddd34ced3813bc
PROJECT272024 <projectrelatedwork27@gmail.com>
2024-04-23 23:46:27 +0530

    adding some control on middleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,18 @@
 export {default} from "next-auth/middleware";
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
+import { getSession } from 'next-auth/react'
 
-export function middleware(request) {
+export  async function middleware(req) {
+  const session = await getSession({ req })
+
+  if (!session) {
+    return NextResponse.redirect('/')
+  }
+
+  // If the user is authenticated, continue to the requested page
+  return NextResponse.next()
+}
+/*export function middleware(request) {
     if (request.nextUrl.pathname.startsWith('/_next')) {
       const locale = request.nextUrl.locale || i18n.defaultLocale
       request.nextUrl.searchParams.set('lang', locale)
@@ -10,5 +21,5 @@ export function middleware(request) {
     }
     return NextResponse.next()
   }
-
+*/
 export const config = {matcher:["/homestay/:path*",'/user/:path*']};
